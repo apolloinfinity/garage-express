832befc3720aca494814affe3852f3186b0cf5ed
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2020-05-12 15:02:14 -0700

    Imported Engine Schema to be used in the addCar function

--- controllers/cars.controller.js
+++ controllers/cars.controller.js
@@ -1,37 +1,45 @@
-const Car = require('../models/car.models');
+const { Car, Engine } = require('../models/car.models');
 const User = require('../models/user.models');
 
 exports.getAllCars = async (req, res) => {
-  const cars = await Car.find({});
-  res.render('cars', {
-    pageTitle: 'Cars',
-    cars: cars,
-  });
+	const cars = await Car.find({});
+	res.render('cars', {
+		pageTitle: 'Cars',
+		cars: cars,
+	});
 };
 
 exports.addCar = async (req, res) => {
-  try {
-    // Get params
-    const { id } = req.params;
-    // console.log(id);
-    // Get and find user
-    const user = await User.findOne({ _id: id });
-    // Create new car with model
-    const newCar = new Car(req.body);
+	try {
+		// Get params
+		const { id } = req.params;
+		// console.log(id);
+		// Get and find user
+		const user = await User.findOne({ _id: id });
+		// Create new car with model
+		const { make, model, year, owner, cylinders, cylinder_configuration, horsepower } = await req.body;
+		console.log(req.body);
+		const newCar = new Car({
+			make,
+			model,
+			year,
+			engine: { cylinders, cylinder_configuration, horsepower },
+			owner,
+		});
 
-    // Assign owner in newCar to user
-    newCar.owner = user._id;
+		// Assign owner in newCar to user
+		newCar.owner = user._id;
 
-    // save car
-    await newCar.save();
+		// save car
+		await newCar.save();
 
-    // Add car to owner's  array
-    user.cars.push(newCar);
+		// Add car to owner's  array
+		user.cars.push(newCar);
 
-    console.log(newCar);
-    await user.save();
-    res.status(201).redirect(`/users/${id}`);
-  } catch (err) {
-    console.error(err);
-  }
+		// console.log(newCar);
+		await user.save();
+		res.status(201).redirect(`/users/${id}`);
+	} catch (err) {
+		console.error(err);
+	}
 };
