9e504f927bfd3a3c233d7ed4fb82200d191591ef
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2020-05-12 15:00:50 -0700

    Created a subdocument for engine in car model file

--- models/car.models.js
+++ models/car.models.js
@@ -1,24 +1,38 @@
 const { Schema, model } = require('mongoose');
 
+const EngineSchema = new Schema(
+	{
+		cylinders: Number,
+		cylinder_configuration: String,
+		horsepower: Number,
+	},
+	{ _id: false }
+);
+
 const CarSchema = new Schema({
-  make: {
-    type: String,
-    required: true,
-  },
-  model: {
-    type: String,
-    require: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
+	make: {
+		type: String,
+		required: true,
+	},
+	model: {
+		type: String,
+		require: true,
+	},
+	year: {
+		type: Number,
+		required: true,
+	},
+	engine: EngineSchema,
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+	},
 });
 
 const Car = model('Car', CarSchema);
+const Engine = model('Engine', EngineSchema);
 
-module.exports = Car;
+module.exports = {
+	Car,
+	Engine,
+};
