3e7a43d1d6c7497cdafca78fb147b9b748faa0c1
Javier Ramirez <apollo.infinity88@gmail.com>
2020-11-21 20:11:32 -0800

    Updating modules.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Garage Demo
+I love cars. I love everything about their design, the engineering, the engines/motors, and what their energy source is. 
+I have much love for the old tried and true gasoline/diesel engines but I absolutely love the new hybrid and fully electric supercars. 
+
+This project is based on a dream of maybe, hopefully owning either good ole' American Muscle car or something like a Tesla Model S. Basically it's a wish list.
+
+## Technologies
+Previous iteration of this project was based on using Mongodb for storing data. MongoDB is great at what it does but I felt that this project could definitely benefit greatly with SQL's relational methods.
+
+* Node.js/Express
+* Knex.js
+* Angular 11+ for Front-end
+* TailwindCSS
 
 Moving from MongoDB to SQL using Knex.js
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const favicon = require('express-favicon');
-const mongoose = require('mongoose');
 
 require('dotenv').config();
 require('./models/car.models');
@@ -10,30 +9,22 @@ require('./models/user.models');
 
 const app = express();
 
-const db = process.env.MONGO_URI;
 const port = process.env.PORT || 80;
 const garage = require('./app.routes');
 
 const start = async () => {
 	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-			dbName: 'garage',
-		});
-		console.log(`Database connection open to ${mongoose.connection.host} ${mongoose.connection.name}`);
-
 		app.use(favicon(__dirname + '/public/favicon.ico'));
 		app.use(express.json());
 		app.use(express.urlencoded({ extended: true }));
 
-		app.set('view engine', 'ejs');
-		app.set('views', 'views');
-
 		app.use(express.static(path.join(__dirname, 'public')));
-		app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
+		app.use(
+			'/css',
+			express.static(
+				path.join(__dirname, '/node_modules/bootstrap/dist/css/')
+			)
+		);
 
 		app.use('/', garage);
 
