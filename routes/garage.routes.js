f337a9697434fb593c66ff7f91656053afe86ce7
Javier Ramirez <apollo.infinity88@gmail.com>
2020-11-25 08:20:33 -0800

    Added put route to update user

--- routes/garage.routes.js
+++ routes/garage.routes.js
@@ -4,12 +4,16 @@ const router = Router();
 const {
 	getUsers,
 	getUser,
+	createUser,
+	updateUser,
 	getCars,
 } = require('../controllers/garage.controllers');
 
 // Users
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser); // use req.params to get id value
 
 // Cars
 router.get('/cars', getCars);
