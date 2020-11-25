6b845dd57fe4903430f564e1b0d2b19a077c1cdf
Javier Ramirez <apollo.infinity88@gmail.com>
2020-11-25 09:15:12 -0800

    Added new http request from controller to routes file

--- routes/garage.routes.js
+++ routes/garage.routes.js
@@ -6,6 +6,7 @@ const {
 	getUser,
 	createUser,
 	updateUser,
+	deleteUser,
 	getCars,
 } = require('../controllers/garage.controllers');
 
@@ -14,6 +15,7 @@ router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.post('/users', createUser);
 router.put('/users/:id', updateUser); // use req.params to get id value
+router.delete('/users/:id', deleteUser); // use req.params to get id value
 
 // Cars
 router.get('/cars', getCars);
