b9c5e8a709dafe82e867c9b2e04816d46b50a459
Javier Ramirez <apollo.infinity88@gmail.com>
2020-12-04 14:35:04 -0800

    Added new controller functions for registration and login to the garage route. Also added the authorized function for route protection

--- routes/garage.routes.js
+++ routes/garage.routes.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 
 const router = Router();
+const { register, login } = require('../controllers/account.controller');
+
+const { authorized } = require('../auth/authorization');
 const {
 	getUsers,
 	getUser,
@@ -14,6 +17,13 @@ const {
 	deleteCar,
 } = require('../controllers/garage.controllers');
 
+// Account Creation and login
+
+router.post('/accounts/register', register);
+router.post('/accounts/login', login);
+
+// Checks all routes after the router.use to see if there is an auth token
+router.use(authorized);
 // Users
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
